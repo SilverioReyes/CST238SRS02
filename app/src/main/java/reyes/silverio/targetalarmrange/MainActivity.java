e9f2ed2deb4929a619d5980991fad41b9a2e9b07
Silverio Reyes <32918848+SilverioReyes@users.noreply.github.com>
2018-07-22 18:05:01 -0700

    Added project dependencies
    Forked project and created project folder and added dependencies.

---
+++ app/src/main/java/reyes/silverio/targetalarmrange/MainActivity.java
@@ -0,0 +1,25 @@
+/*************************************************************
+ * Author:          Silverio Reyes
+ * Filename:        MainActivity.java
+ * Organization:    Oregon Institute of Technology
+ * Class:           CST238 GUI
+ *
+ * Date Created:    7/01/18 - Mitch Besser-Laber Created .gitignore,
+ *                            README.md, and REPORT.md files
+ *
+ * Date Modified:   7/22/18 - Created project and
+ *                            added dependencies
+ **************************************************************/
+package reyes.silverio.targetalarmrange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
